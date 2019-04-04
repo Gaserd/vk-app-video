df52301ebee647b1d381dae7b51c05a0efd6ebbd
gaserd <gaserd123@gmail.com>
2019-04-04 14:23:06 +0300

    Updates

---
+++ precache-manifest.2e58be9040d1496430fbd63ecc180c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "618198107aff472e5679",
+    "url": "/vk-app-video/static/js/runtime~main.c739c80b.js"
+  },
+  {
+    "revision": "712d6e72dd1037f8ebe3",
+    "url": "/vk-app-video/static/js/main.7947a127.chunk.js"
+  },
+  {
+    "revision": "f4e32e28bdc3fcfdcee8",
+    "url": "/vk-app-video/static/js/2.64b4322a.chunk.js"
+  },
+  {
+    "revision": "f4e32e28bdc3fcfdcee8",
+    "url": "/vk-app-video/static/css/2.88b9f546.chunk.css"
+  },
+  {
+    "revision": "def22019c2649b2e9a22f815b75fd3a5",
+    "url": "/vk-app-video/index.html"
+  }
+];
